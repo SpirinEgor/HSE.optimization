a26d27aca53ae71d5fc358cce1524ca524f6e803
Egor Spirin <spirin.egor@mail.ru>
2020-10-13 22:21:04 +0300

    Rewrite main to test on all given oracles

--- main.py
+++ main.py
@@ -1,22 +1,55 @@
+from dataclasses import dataclass
+
 import numpy
 
-import oracle
 from optimize import Brent
+from oracle import Oracle, unimodal
+from utils import CountCallsWrapper
+
+LINE_DELIMITER = "=" * 100
+
+
+@dataclass
+class OptimizeParameters:
+    left_bound: float
+    right_bound: float
+    epsilon: float = 1e-8
+
+
+def test_optimize(brent: Brent, params: OptimizeParameters, oracle: Oracle):
+    oracle_function = oracle.get_oracle()
+    count_call_wrapper = CountCallsWrapper(oracle_function)
+    optimize_result = brent.brent_with_derivatives(
+        count_call_wrapper, params.left_bound, params.right_bound, params.epsilon
+    )
+    x_min = optimize_result.x_min
+    x_min_true = oracle.get_x_min()
+    error = numpy.abs(x_min - oracle.get_x_min())
+    error = 0 if error < params.epsilon else error
+    print(LINE_DELIMITER)
+    print(f"Optimized function: {oracle.get_name()}\n"
+          f"Reached minimum: x={x_min} (f(x), f'(x) = {oracle_function(x_min)})\n"
+          f"   True minimum: x={x_min_true} (f(x), f'(x) = {oracle_function(x_min_true)})\n"
+          f"Error: {error}\n"
+          f"Number of calls to the oracle: {count_call_wrapper.get_number_of_calls()}\n"
+          f"Optimization history: {' '.join(map(str, optimize_result.optimize_history))}")
+    print(LINE_DELIMITER)
 
 
 def main():
-    true_x_min = 0
-    left_bound, right_bound = -10, 10
-    epsilon = 1e-8
+    test_samples = [
+        (OptimizeParameters(-10, 10), unimodal.Example()),
+        (OptimizeParameters(-10, 10), unimodal.Quadratic(5)),
+        (OptimizeParameters(0, 3), unimodal.Function4()),
+        (OptimizeParameters(-1, 2), unimodal.Function13()),
+        (OptimizeParameters(0, 6), unimodal.Function18())
+    ]
+
     max_iterations = 100_000
-
-    oracle_function = oracle.unimodal.quadratic(true_x_min)
-    oracle_wrapper = oracle.OracleWrapper(oracle_function)
-
     brent = Brent(max_iterations)
-    x_min = brent.brent_with_derivatives(oracle_wrapper, left_bound, right_bound, epsilon)
-    print(f"Optimized minimum is {x_min}, true minimum is {true_x_min} (error: {numpy.abs(x_min - true_x_min)})\n"
-          f"Number of calls to the oracle is {oracle_wrapper.get_number_of_calls()}")
+
+    for optimizer_params, oracle in test_samples:
+        test_optimize(brent, optimizer_params, oracle)
 
 
 if __name__ == "__main__":

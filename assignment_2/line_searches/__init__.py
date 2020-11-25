a09e89a5d580764f1a2363e7a81ae9b690c52b64
Egor Spirin <spirin.egor@mail.ru>
2020-11-25 22:36:18 +0300

    rename line search methods files

--- assignment_2/line_searches/__init__.py
+++ assignment_2/line_searches/__init__.py
@@ -1,9 +1,9 @@
 from .abstract_line_search import AbstractLineSearch
-from .golden_line_search import GoldenLineSearch
-from .brent_line_search import BrentLineSearch
-from .armijo_line_search import ArmijoLineSearch
-from .wolfe_line_search import WolfeLineSearch
-from .nesterov_line_search import NesterovLineSearch
+from .golden import GoldenLineSearch
+from .brent import BrentLineSearch
+from .armijo import ArmijoLineSearch
+from .wolfe import WolfeLineSearch
+from .nesterov import NesterovLineSearch
 from .line_search_maker import make_line_search
 
 __all__ = [
